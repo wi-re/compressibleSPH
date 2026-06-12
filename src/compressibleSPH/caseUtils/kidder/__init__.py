4d959bc6da600522f56c959538bab337fad63e8e
Rene <rene.winchenbach@gmail.com>
2026-06-12 13:17:09 +0100

    cleanup kidder (cant pickle)

---
+++ src/compressibleSPH/caseUtils/kidder/__init__.py
@@ -0,0 +1,5 @@
+from .bc import buildKidderBCs
+from .sample import buildKidder
+from .kidder import KidderIsentropicCapsuleAnalyticSolution
+
+__all__ = ['buildKidderBCs', 'buildKidder', 'KidderIsentropicCapsuleAnalyticSolution']
