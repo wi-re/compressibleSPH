52fdd0f1c43d1624e7b722b854d6efdeb88c2222
Rene <rene.winchenbach@gmail.com>
2026-07-01 15:57:40 +0100

    add publication scripts

--- src/compressibleSPH/__init__.py
+++ src/compressibleSPH/__init__.py
@@ -1,5 +1,5 @@
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 
 # from .casefile import argparse_defaults_from_casefile, build_configs_from_casefile, load_casefile
 # from .shape_generation import populateSourceObstacleGridsStructured, sampleShapeStructured
