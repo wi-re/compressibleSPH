15306bebc3b84bf3411aac67c2ae00623a0555b4
rene@home <rene.winchenbach@gmail.com>
2026-07-24 11:05:07 +0100

    add dataset cases

--- datagen/weaklyCompressible/03-dambreak.py
+++ datagen/weaklyCompressible/03-dambreak.py
@@ -47,14 +47,14 @@ import argparse
 parser = argparse.ArgumentParser(description='Run the dam break simulation with obstacle.')
 
 parser.add_argument('--nx', type=int, default=128, help='Number of particles along the x-axis')
-parser.add_argument('--markerSize', type=int, default=8, help='Size of the markers in the plot')
+parser.add_argument('--markerSize', type=int, default=2, help='Size of the markers in the plot')
 parser.add_argument('--plotWidth', type=int, default=28, help='Width of the plot in inches')
 parser.add_argument('--n_h', type=int, default=4, help='Target number of neighbors')
 parser.add_argument('--L', type=float, default=2.0, help='Length of the domain')
 parser.add_argument('--W', type=float, default=4.0, help='Width of the domain')
 
-parser.add_argument('--fluidWidth', type=float, default=2.0 * 5.0 / 6.0, help='Width of the fluid region') # L * 5/6
-parser.add_argument('--fluidHeight', type=float, default=2.0 / 3.0, help='Height of the fluid region') # L / 3
+# parser.add_argument('--fluidWidth', type=float, default=2.0 * 5.0 / 6.0, help='Width of the fluid region') # L * 5/6
+# parser.add_argument('--fluidHeight', type=float, default=2.0 / 3.0, help='Height of the fluid region') # L / 3
 
 parser.add_argument('--timeLimit', type=float, default=4.0, help='Time limit for the simulation')
 parser.add_argument('--enableFreestream', action='store_true', help='Enable freestream boundary conditions')
@@ -76,12 +76,19 @@ parser.add_argument('--enableSloshing', action='store_true', help='Enable sloshi
 parser.add_argument('--sloshingAmplitude', type=float, default=0.1, help='Amplitude of sloshing motion (default: 0.1)')
 parser.add_argument('--sloshingFrequency', type=float, default=1.0, help='Frequency of sloshing motion (default: 1.0)')
 
-parser.add_argument('--enableObstacle', action='store_true', help='Enable obstacle in the simulation')
-parser.add_argument('--obstacleOffsetX', type=float, default=3.0/4.0, help='X offset of the obstacle (default: 0.0)') # W/4
+parser.add_argument('--obstacleActive', action='store_true', help='Enable obstacle in the simulation')
+parser.add_argument('--obstacleType', type=str, default='circle', help='Type of obstacle to include (none, circle, ellipse, box, roundedBox, equilateralTriangle, hexagon, horseshoe, star, nacaXXXX)')
+
+parser.add_argument('--offsetX', type=float, default=3.0/4.0, help='X offset of the obstacle (default: 0.0)') # W/4
 # parser.add_argument('--obstacleOffsetY', type=float, default=0.0, help='Y offset of the obstacle (default: 0.0)')
-parser.add_argument('--obstacleAngle', type=float, default=0.0, help='Angle of the obstacle (default: 0.0)')
-parser.add_argument('--obstacleWidth', type=float, default=1.0/16.0, help='Width of the obstacle (default: 1.0/16.0)') # L/16
-parser.add_argument('--obstacleHeight', type=float, default=1.0/4.0, help='Height of the obstacle (default: 1.0/4.0)') # L/4
+parser.add_argument('--aoa', type=float, default=0.0, help='Angle of the obstacle (default: 0.0)')
+parser.add_argument('--maxExtent', type=float, default=1.0/16.0, help='Width of the obstacle (default: 1.0/16.0)') # L/16
+# parser.add_argument('--obstacleHeight', type=float, default=1.0/4.0, help='Height of the obstacle (default: 1.0/4.0)') # L/4
+
+parser.add_argument('--fillRatio', type=float, default=1.0/3.0, help='Fill ratio for the domain')
+parser.add_argument('--semiPeriodic', action='store_true', help='Enable semi-periodic boundary conditions')
+parser.add_argument('--fullyPeriodic', action='store_true', help='Enable fully periodic boundary conditions')
+parser.add_argument('--fluidWidth', type=float, default=5/2 * 1/3, help='Width of the fluid region (default: 4.0)')
 
 args = parser.parse_args()
 
@@ -103,8 +110,8 @@ nu_visc = 0.0005
 freeSurface = True
 
 timestamp = getCurrentTimestamp()
-obstacleText = f'obstacle_{args.obstacleWidth:.4g}_{args.obstacleHeight:.4g}_{args.obstacleAngle:.4g}_{args.obstacleOffsetX:.4g}' if args.enableObstacle else 'no_obstacle'
-caseName = f'{args.caseName}_{timestamp}_{nx}_{n_h}_{L}_{W}_{obstacleText}'
+obstacleText = f'obstacle_{args.maxExtent:.4g}_{args.aoa:.4g}_{args.offsetX:.4g}' if args.obstacleActive else 'no_obstacle'
+caseName = f'{args.caseName}/{timestamp}_{nx}_{n_h}_{L}_{W}_{obstacleText}'
 
 extraData = {
     'nx': nx,
@@ -120,10 +127,20 @@ extraData = {
 device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 dtype = get_torch_precision()
 
-
 domain = buildDomainDescription(L + dx * (band) * 2, dim, True, device, dtype)
 domain.min = torch.tensor([-W/2 - dx * (band), -L/2 - dx * (band)], device = device, dtype = dtype)
 domain.max = torch.tensor([W/2 + dx * (band), L/2 + dx * (band)], device = device, dtype = dtype)
+
+# Semi periodic
+if args.semiPeriodic:
+    domain.min = torch.tensor([-W/2, -L/2 - dx * (band)], device = device, dtype = dtype)
+    domain.max = torch.tensor([W/2, L/2 + dx * (band)], device = device, dtype = dtype)
+
+# Closed domain
+if args.fullyPeriodic:
+    domain.min = torch.tensor([-W/2, -L/2], device = device, dtype = dtype)
+    domain.max = torch.tensor([W/2, L/2], device = device, dtype = dtype)
+
 interiorDomain = buildDomainDescription(L, dim, False, device, dtype)
 interiorDomain.min = torch.tensor([-W/2, -L/2], device = device, dtype = dtype)
 interiorDomain.max = torch.tensor([W/2, L/2], device = device, dtype = dtype)
@@ -171,47 +188,54 @@ domain_sdf = lambda x: sampleDomainSDF(x, interiorDomain, invert = False)
 # obstacleHeight = L/4
 
 from warpSPH.utils.naca import *
-from utils import buildObstacleSDF
+from utils import buildObstacleSDF, build_sdfs, buildPresetObstacles
 
-obstacleWidth = args.obstacleWidth
-obstacleHeight = args.obstacleHeight
-obstacleAngle = args.obstacleAngle * np.pi / 180.0
-
-
-obstacle_sdf = lambda x: getSDF('box')['function'](x, torch.tensor([obstacleWidth/2,obstacleHeight/2]).to(x.device))
+# obstacleWidth = args.obstacleWidth
+# obstacleHeight = args.obstacleHeight
+# obstacleAngle = args.obstacleAngle * np.pi / 180.0
 
 
-translate = lambda sdf, offset: operatorDict['translate'](sdf, torch.tensor(offset).to(device))
-rotate = lambda sdf, angle: operatorDict['rotate'](sdf, angle)
-union = lambda sdf1, sdf2: operatorDict['union'](sdf1, sdf2)
+# obstacle_sdf = lambda x: getSDF('box')['function'](x, torch.tensor([obstacleWidth/2,obstacleHeight/2]).to(x.device))
 
 
-downShift = L/2 - obstacleHeight/2 + obstacleWidth * math.sin(abs(obstacleAngle)) *2
-rightShift = args.obstacleOffsetX
+# translate = lambda sdf, offset: operatorDict['translate'](sdf, torch.tensor(offset).to(device))
+# rotate = lambda sdf, angle: operatorDict['rotate'](sdf, angle)
+# union = lambda sdf1, sdf2: operatorDict['union'](sdf1, sdf2)
 
 
-obstacle_sdf = rotate(obstacle_sdf, obstacleAngle)
-obstacle_sdf = translate(obstacle_sdf, [rightShift, -downShift])
-merged_sdf = lambda x: domainSDF(x, interiorDomain, invert = False)
-if args.enableObstacle:
-    merged_sdf = union(merged_sdf, obstacle_sdf)
-
-# merged_sdf = translate(obstacle_sdf, [L/2, -L/4])
-domain_sdf = lambda x: sampleSDF(x, merged_sdf, invert=False)
+# downShift = L/2 - obstacleHeight/2 + obstacleWidth * math.sin(abs(obstacleAngle)) *2
+# rightShift = args.obstacleOffsetX
 
 
-fluidW = args.fluidWidth
-fluidH = args.fluidHeight
+# obstacle_sdf = rotate(obstacle_sdf, obstacleAngle)
+# obstacle_sdf = translate(obstacle_sdf, [rightShift, -downShift])
+# merged_sdf = lambda x: domainSDF(x, interiorDomain, invert = False)
+# if args.enableObstacle:
+#     merged_sdf = union(merged_sdf, obstacle_sdf)
+
+# # merged_sdf = translate(obstacle_sdf, [L/2, -L/4])
+# domain_sdf = lambda x: sampleSDF(x, merged_sdf, invert=False)
+
+presets = buildPresetObstacles(args.maxExtent, args.offsetX, args.L, args.fillRatio, args.aoa)
+obstacle = presets.get(args.obstacleType)
+
+regions, fluid_sdf, domain_sdf, obstacle_sdf = build_sdfs(config, schemeConfig, args.band, args, domain, interiorDomain, obstacle)
+
+fluidW = args.fluidWidth * W
+fluidH = args.fillRatio * L
+
 box_sdf = lambda points: sampleSDF(points, operatorDict['translate'](lambda x: getSDF('box')['function'](x, torch.tensor([fluidW/2,fluidH/2]).to(points.device)), torch.tensor([interiorDomain.min[0]+fluidW/2,interiorDomain.min[1] + fluidH/2]).to(points.device)), invert = False)
+
+# domain_sdf = lambda points: sampleSDF(points, domain_sdf, invert = False)
 
 regions = []
 regions.append(buildRegion(config, schemeConfig, domain_sdf, RegionType.Boundary, initialConditions = {}, kind = BCType.constant))
-regions.append(buildRegion(config, schemeConfig, box_sdf, RegionType.Fluid, initialConditions = {}))
+# regions.append(buildRegion(config, schemeConfig, box_sdf, RegionType.Fluid, initialConditions = {}))
 
 # fluid_sdf = lambda x: sampleDomainSDF(x, domain, invert = True)
 # regions = []
 
-# regions.append(buildRegion(config, schemeConfig, fluid_sdf, RegionType.Fluid, initialConditions = {}))
+regions.append(buildRegion(config, schemeConfig, box_sdf, RegionType.Fluid, initialConditions = {}))
 
 for region in regions:
     region = filterRegion(region, regions)
@@ -235,6 +259,86 @@ compressibleSystem = initializeWeaklyCompressibleSimulation(regions, config, sch
 # compressibleSystem.state.velocities[:,0] =  u_mag * torch.cos(ktgv * compressibleSystem.state.positions[:,0] * np.pi + phaseShift_x) * torch.sin(ktgv * np.pi * compressibleSystem.state.positions[:,1] + phaseShift_y)
 # compressibleSystem.state.velocities[:,1] = -u_mag * torch.sin(ktgv * compressibleSystem.state.positions[:,0] * np.pi + phaseShift_x) * torch.cos(ktgv * np.pi * compressibleSystem.state.positions[:,1] + phaseShift_y)
 
+
+if args.enableFreestream:
+    u_freestream = args.freeStreamVelocity
+    forcingWidth = args.forcingWidth
+
+    forcingSDF = lambda points: sampleSDF(points, lambda x: getSDF('box')['function'](x, torch.tensor([W/2 - forcingWidth, L/2]).to(points.device)), invert = True)
+
+    def ldcDirichlet(state, cfg, schemeCfg, positions, d, n, t, dt):
+        velocities = state.velocities.clone()
+        # mask = torch.logical_or(positions[:,0] < -W/2 + forcingWidth, positions[:,0] > W/2 - forcingWidth)
+        velocities[:,0] = u_freestream * 2
+        # v_diff = u_freestream - state.velocities[:,0]
+        # # slowly ramp the velocities to the target velocity over time
+        # velocities[:,0] = state.velocities[:,0] + v_diff * dt / 0.1
+        return velocities
+
+    def ldcDirichletDensity(state, cfg, schemeCfg, positions, d, n, t, dt):
+        densities = state.densities.clone()
+        densities[:] = rho0
+        # ramp the densities to the target density over time
+        densities[:] = state.densities[:] + (rho0 - state.densities[:]) * dt / 0.1
+        return densities
+
+    def ldcDirichletUpdate(state, cfg, schemeCfg, positions, d, n, t, dt):
+        velocities = torch.zeros_like(state.velocities)
+        mask = torch.logical_or(positions[:,0] < -W/2 + forcingWidth, positions[:,0] > W/2 - forcingWidth)
+        velocities[:,0] = torch.where(mask, 0.0, velocities[:,0])
+        return velocities
+
+    def ldcDirichletUpdateDensity(state, cfg, schemeCfg, positions, d, n, t, dt):
+        densities = torch.zeros_like(state.densities)
+        mask = torch.logical_or(positions[:,0] < -W/2 + forcingWidth, positions[:,0] > W/2 - forcingWidth)
+        densities[:] = torch.where(mask, 0.0, densities[:])
+        return densities
+
+    def ldcForcing(state, cfg, schemeCfg, positions, d, n, t, dt):
+        forcing = torch.zeros_like(state.velocities)
+        mask = torch.logical_or(positions[:,0] < -W/2 + forcingWidth, positions[:,0] > W/2 - forcingWidth)
+        velocities = state.velocities.clone()
+        velocities[:,0] = u_freestream
+        v_diff = u_freestream - state.velocities[:,0]
+        # slowly ramp the velocities to the target velocity over time
+        velocities[:,0] = state.velocities[:,0] + v_diff * dt / 0.1
+
+        forcing[:,0] = v_diff * dt / 0.1
+
+
+
+
+        return forcing
+
+    ldcBC = BoundaryCondition(
+        type = BoundaryConditionType.dynamic,
+        sdf = forcingSDF,
+        dirichletFunctions = {
+            # 'velocities': lambda state, cfg, schemeCfg, positions, d, n, t, dt: ldcDirichlet(state, cfg, schemeCfg, positions, d, n, t, dt),
+            # 'densities': lambda state, cfg, schemeCfg, positions, d, n, t, dt: ldcDirichletDensity(state, cfg, schemeCfg, positions, d, n, t, dt)
+        },
+        updateFunctions = {
+            # 'dvdt': lambda state, cfg, schemeCfg, positions, d, n, t, dt: ldcDirichletUpdate(state, cfg, schemeCfg, positions, d, n, t, dt),
+            # 'drhodt': lambda state, cfg, schemeCfg, positions, d, n, t, dt: ldcDirichletUpdateDensity(state, cfg, schemeCfg, positions, d, n, t, dt)
+        },
+        forcingFunctions = [ldcForcing]
+
+    )
+    schemeConfig.boundaryConditions = [ldcBC]
+    minBoundaryDistance = torch.ones_like(compressibleSystem.state.positions[:,0]) * np.inf
+    for region in regions:
+        if region.type == RegionType.Boundary:
+            distances = region.sdf(compressibleSystem.state.positions)[0]
+            minBoundaryDistance = torch.min(minBoundaryDistance, distances)
+    maxDistance = compressibleSystem.state.supports.max() * 2.0
+    minBoundaryDistance = torch.clamp(minBoundaryDistance, min=0.0, max = maxDistance)
+    ramp = (minBoundaryDistance) / maxDistance
+    def rampFn(r):
+        ramped = 15/8 * r - 10/8 * r**3 + 3/8 * r**5
+        return torch.clamp(ramped, min=0.0, max = 1.0)
+    ramped = rampFn(ramp)
+
+    compressibleSystem.state.velocities[compressibleSystem.state.kinds == 0,0] += u_freestream * ramped[compressibleSystem.state.kinds == 0]
 
 
 schemeConfig.fluid.fixedSoundSpeed, config.dt = setupWeaklyCompressibleTimestep(config, schemeConfig, compressibleSystem, targetDt, verbose = True)
@@ -261,7 +365,7 @@ exportSimulationSystem(exportPath, 'initialState', scheme, compressibleSystem, e
 }, **extraData))
 
 if args.plot:
-    titleString = f'{args.caseName} | t = {runningState.t:.4g}/{args.timeLimit:.4g} | dt = {config.dt:.4g} | particles = {len(runningState.state.positions)} | nx = {nx} | n_h = {n_h} | L = {L} | W = {W} | obstacle: {args.enableObstacle}'
+    titleString = f'{args.caseName} | t = {runningState.t:.4g}/{args.timeLimit:.4g} | dt = {config.dt:.4g} | particles = {len(runningState.state.positions)} | nx = {nx} | n_h = {n_h} | L = {L} | W = {W} | obstacle: {args.obstacleActive}'
 
     markerSize = args.markerSize
     plotter = visualize(
@@ -285,7 +389,9 @@ if args.plot:
                 # gridVisualization = GridVisualization(
                 #     resolution = 512,
                 # ),
-                # vMin=1e-10
+                # vMin=1e-10,
+                vMin = 0.0,
+                vMax = schemeConfig.fluid.fixedSoundSpeed * 0.1,
             ),
             "B": PlottingOptions(
                 colorMap = DivergingColorMap.RdBu,
@@ -335,7 +441,7 @@ if args.plot:
 
 
 
-schemeConfig.diffusionParams.inviscid = False
+schemeConfig.diffusionParams.inviscid = True
 schemeConfig.diffusionParams.viscidNu = 0.01
 
 nu = schemeConfig.diffusionParams.viscidNu if schemeConfig.diffusionParams.inviscid == False else alphaToNu(schemeConfig.diffusionParams.inviscidAlpha, schemeConfig.fluid.fixedSoundSpeed, compressibleSystem.state.supports.mean().cpu().item(), config.dim)
@@ -387,7 +493,7 @@ for i in (tq := tqdm(range(nSteps), leave = False)):
 
     if args.plot and plotter is not None:
         if i % 10 == 0 and i > 0:
-            titleString = f'{args.caseName} | t = {runningState.t:.4g}/{args.timeLimit:.4g} | dt = {config.dt:.4g} | particles = {len(runningState.state.positions)} | nx = {nx} | n_h = {n_h} | L = {L} | W = {W} | obstacle: {args.enableObstacle} | max vel: {torch.linalg.norm(runningState.state.velocities, dim = -1).max():.3g} | iter time: {timing:.3f} ms'
+            titleString = f'{args.caseName} | t = {runningState.t:.4g}/{args.timeLimit:.4g} | dt = {config.dt:.4g} | particles = {len(runningState.state.positions)} | nx = {nx} | n_h = {n_h} | L = {L} | W = {W} | obstacle: {args.obstacleActive} | max vel: {torch.linalg.norm(runningState.state.velocities, dim = -1).max():.3g} | iter time: {timing:.3f} ms'
             plotter.updateTitle(titleString)
             plotter.updateQuantities(
                 {
